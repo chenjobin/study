98e0e2a7759e8c9cb60b96530df995845c99c8c5
chenjobin <923869988@qq.com>
2017-08-23 09:50:52 +0800

    V0.3.8 添加头像字段，利用上版本所说的静态资源访问问题， 虽然还未解决，但media倒是解决了

--- users/models.py
+++ users/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from types import MethodType #类动态绑定方法
+import os
+
+AVATAR_ROOT = 'avatar'
+AVATAR_DEFAULT = os.path.join(AVATAR_ROOT, 'default.png')
+
 # 找回密码创建
 class User_ex(models.Model):
     """User models ex"""
@@ -10,3 +16,19 @@ class User_ex(models.Model):
  
     def __str__(self):
         return u'%s' % (self.valid_code)
+
+class User_Avatar(models.Model):
+    """user avatar"""
+    user = models.ForeignKey(User)
+    avatar = models.ImageField(upload_to='avatar/')
+
+#动态绑定头像相关的方法
+def get_avatar_url(self):
+    try:
+        avatar = User_Avatar.objects.get(user=self.id)
+        return avatar.avatar
+    except Exception as e:
+        return AVATAR_DEFAULT
+
+#动态绑定方法
+User.get_avatar_url = MethodType(get_avatar_url, User)
