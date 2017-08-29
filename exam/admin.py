a5dda401f7ed28b8b05605971d9a1aaee155d611
chenjobin <923869988@qq.com>
2017-08-29 21:14:26 +0800

    V0.5.9 增加填空题模型。然后填空题的答案字段，还需要仔细考虑，改进

--- exam/admin.py
+++ exam/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Exam_Topic,Single_Q
+from .models import Exam_Topic,Single_Q,Fill_Q
 
 class Exam_TopicAdmin(admin.ModelAdmin):
     list_display = ('caption','id','date_added', 'date_update')
@@ -10,9 +10,15 @@ class Exam_TopicAdmin(admin.ModelAdmin):
 class Single_QAdmin(admin.ModelAdmin):
     list_display = ('caption','id','author','topic','date_added', 'date_update','recommend')
     list_per_page = 15
-    fields = (('topic', 'author','recommend'), 'tags','caption','title','answer_detail',
-              'select_correct',('select_2','select_3','select_4'))
+    fields = (('topic', 'author','recommend'), 'tags','caption','title',
+              'answer',('select_2','select_3','select_4'),'answer_detail')
 
+
+class Fill_QAdmin(admin.ModelAdmin):
+    list_display = ('caption','id','author','topic','date_added', 'date_update','recommend')
+    list_per_page = 15
+    fields = (('topic', 'author','recommend'), 'tags',('caption','blank_num'),'title','answer','answer_detail')
 
 admin.site.register(Exam_Topic,Exam_TopicAdmin)
 admin.site.register(Single_Q,Single_QAdmin)
+admin.site.register(Fill_Q,Fill_QAdmin)
--- exam/models.py
+++ exam/models.py
@@ -34,11 +34,10 @@ class Single_Q(models.Model):
     title = UEditorField('题目', height=300, width=1000,
         default=u'', blank=True, imagePath="exam/uploads/images/",
         toolbars='besttome', filePath='exam/uploads/files/')
-    select_correct = models.TextField('正确选项',max_length=200,default=u'')
+    answer = models.TextField('正确选项',max_length=200,default=u'')
     select_2 = models.TextField('选项2',max_length=200,default=u'')
     select_3 = models.TextField('选项3',max_length=200,default=u'')
     select_4 = models.TextField('选项4',max_length=200,default=u'')
-
     answer_detail = UEditorField('答案解析', height=300, width=1000,
         default=u'', blank=True, imagePath="exam/uploads/images/",
         toolbars='besttome', filePath='exam/uploads/files/')
@@ -57,5 +56,36 @@ class Single_Q(models.Model):
         else:
             return self.caption[:50]
 
+# 填空题
+class Fill_Q(models.Model):
+    '''填空题模型'''
+    topic = models.ForeignKey(Exam_Topic,verbose_name='归属主题')
+    author = models.ForeignKey(User, blank=True, null=True, verbose_name='作者')
+    tags = models.ManyToManyField(Tag,blank=True,verbose_name='标签') #多对多字段，绑定下面的Tag模型
+    recommend = models.BooleanField('重点题目',default=False) #布尔字段，我用于标记是否是重点题
+    # 考虑到HTML的影响，加入简化版，其实是和title一样
+    caption = models.CharField('题目简化版',max_length=200)
+    blank_num=models.PositiveIntegerField(default=1,verbose_name='空白数') # 记录填空的数量
+    title = UEditorField('题目', height=300, width=1000,
+        default=u'', blank=True, imagePath="exam/uploads/images/",
+        toolbars='besttome', filePath='exam/uploads/files/')
+    answer=models.TextField('正确答案',max_length=200,default=u'')
+    answer_detail = UEditorField('答案解析', height=300, width=1000,
+        default=u'', blank=True, imagePath="exam/uploads/images/",
+        toolbars='besttome', filePath='exam/uploads/files/')
 
+
+    date_added = models.DateTimeField('发布时间',auto_now_add=True)
+    date_update = models.DateTimeField('更新时间',auto_now=True)
+
+    class Meta:
+        verbose_name = '填空题'
+        verbose_name_plural = '填空题'
+
+    def __str__(self):
+        '''返回模型的字符串表示'''
+        if len(self.caption)>50:
+            return self.caption[:50] + '...'
+        else:
+            return self.caption[:50]
 
