45ce90a98375e2ba9baf9427e4f02c471c86c066
chenjobin <923869988@qq.com>
2017-09-15 12:18:28 +0800

    V0.7.2 exam增加了二级主题，相关模型也做了调整

--- exam/admin.py
+++ exam/admin.py
@@ -1,16 +1,20 @@
 from django.contrib import admin
-from .models import Exam_Topic,Single_Q,Fill_Q,Fill_Answer,SingleWrongAnswer
+from .models import Exam_Topic,Exam_Topic_Second,Single_Q,Fill_Q,Fill_Answer,SingleWrongAnswer
 
 class Exam_TopicAdmin(admin.ModelAdmin):
     list_display = ('caption','id','date_added', 'date_update')
     # list_per_page = 15
     # fields = (('topic', 'author'), 'tags',('recommend','top_in'),'title','text')
 
+class Exam_Topic_SecondAdmin(admin.ModelAdmin):
+    list_display = ('caption','id','topic','date_added', 'date_update','recommend')
+    # list_per_page = 15
+    fields = (('topic', 'recommend'), 'caption','description')
 
 class Single_QAdmin(admin.ModelAdmin):
     list_display = ('caption','id','author','topic','date_added', 'date_update','recommend')
     list_per_page = 15
-    fields = (('topic', 'author','recommend'), 'tags','caption','title',
+    fields = (('topic', 'author','recommend'),( 'topic_second','tags'),'caption','title',
               'answer',('select_2','select_3','select_4'),'answer_detail')
 
 class Fill_AnswerInline(admin.TabularInline):
@@ -20,10 +24,11 @@ class Fill_AnswerInline(admin.TabularInline):
 class Fill_QAdmin(admin.ModelAdmin):
     list_display = ('caption','id','author','topic','date_added', 'date_update','recommend')
     list_per_page = 15
-    fields = (('topic', 'author','recommend'), 'tags',('caption','blank_num'),'title','answer_detail')
+    fields = (('topic', 'author','recommend'), ( 'topic_second','tags'),('caption','blank_num'),'title','answer_detail')
     inlines=[Fill_AnswerInline]
 
 admin.site.register(Exam_Topic,Exam_TopicAdmin)
+admin.site.register(Exam_Topic_Second,Exam_Topic_SecondAdmin)
 admin.site.register(Single_Q,Single_QAdmin)
 admin.site.register(Fill_Q,Fill_QAdmin)
 admin.site.register(SingleWrongAnswer)
--- exam/models.py
+++ exam/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from DjangoUeditor.models import UEditorField
-from django.utils.encoding import  python_2_unicode_compatible
 from blog.models import Tag
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
-@python_2_unicode_compatible
 class Exam_Topic(models.Model):
     '''题目的主题'''
     caption = models.CharField('主题',max_length=200)
@@ -23,10 +21,32 @@ class Exam_Topic(models.Model):
         verbose_name_plural = '试题分类'
         # ordering = ['text']  # 按照哪个栏目排序
 
+class Exam_Topic_Second(models.Model):
+    '''题目的细分主题'''
+    topic = models.ForeignKey(Exam_Topic,verbose_name='归属主题')
+    recommend = models.BooleanField('重要主题',default=False) #布尔字段，标记是否是推荐主题
+    caption = models.CharField('二级主题',max_length=200)
+    date_added = models.DateTimeField('发布时间',auto_now_add=True)
+    date_update = models.DateTimeField('更新时间',auto_now=True)
+    description = models.TextField('相关描述') #描述主题
+
+
+
+    def __str__(self):
+        '''返回模型的字符串表示'''
+        return self.caption
+
+    class Meta:
+        verbose_name = '试题二级分类' #后台admin  add主题
+        verbose_name_plural = '试题二级分类'
+        # ordering = ['text']  # 按照哪个栏目排序
+
 # 单项选择题
 class Single_Q(models.Model):
     '''单选题模型'''
     topic = models.ForeignKey(Exam_Topic,verbose_name='归属主题')
+    topic_second = models.ManyToManyField(Exam_Topic_Second,blank=True,verbose_name='归属二级主题')
+
     author = models.ForeignKey(User, blank=True, null=True, verbose_name='作者')
     tags = models.ManyToManyField(Tag,blank=True,verbose_name='标签') #多对多字段，绑定下面的Tag模型
     recommend = models.BooleanField('重点题目',default=False) #布尔字段，我用于标记是否是重点题
@@ -61,6 +81,7 @@ class Single_Q(models.Model):
 class Fill_Q(models.Model):
     '''填空题模型'''
     topic = models.ForeignKey(Exam_Topic,verbose_name='归属主题')
+    topic_second = models.ManyToManyField(Exam_Topic_Second,blank=True,verbose_name='归属二级主题')
     author = models.ForeignKey(User, blank=True, null=True, verbose_name='作者')
     tags = models.ManyToManyField(Tag,blank=True,verbose_name='标签') #多对多字段，绑定下面的Tag模型
     recommend = models.BooleanField('重点题目',default=False) #布尔字段，我用于标记是否是重点题
