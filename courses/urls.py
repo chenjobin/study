9fed04c7bd2a6a0aa043fa67e9ef77a5b24187e2
chenjobin <923869988@qq.com>
2018-01-18 16:22:38 +0800

    V0.9.7.3 添加django-braces,管理课程模块完成，可以利用JQui调整order

--- courses/urls.py
+++ courses/urls.py
@@ -33,13 +33,13 @@ urlpatterns = [
     url(r'^module/(?P<module_id>\d+)/$',
     views.ModuleContentListView.as_view(),
     name='module_content_list'),
-    # # 重新整理模块和内容
-    # url(r'^module/order/$',
-    #    views.ModuleOrderView.as_view(),
-    #    name='module_order'),
-    # url(r'^content/order/$',
-    #    views.ContentOrderView.as_view(),
-    #    name='content_order'),
+    # 重新整理模块和内容
+    url(r'^module/order/$',
+       views.ModuleOrderView.as_view(),
+       name='module_order'),
+    url(r'^content/order/$',
+       views.ContentOrderView.as_view(),
+       name='content_order'),
 
 ]
 
--- courses/views.py
+++ courses/views.py
@@ -16,6 +16,8 @@ from .models import Module, Content
 # # 重新整理模块和内容
 # from django.views.decorators.csrf import csrf_exempt
 # from django.http import JsonResponse
+# django中没有找到替代，所以还是装了braces
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
 class OwnerMixin(object):
     def get_queryset(self):
@@ -173,5 +175,25 @@ class ModuleContentListView(TemplateResponseMixin, View):
         return self.render_to_response({'module': module})
 
 # 重新整理模块和内容
+class ModuleOrderView(CsrfExemptMixin,
+                         JsonRequestResponseMixin,
+                         View):
+
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id,
+                course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+
+class ContentOrderView(CsrfExemptMixin,
+                          JsonRequestResponseMixin,
+                          View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,
+                          module__course__owner=request.user) \
+                          .update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+
 
 
